e4396131dc64a2c480aa120ec3fa713955037125
ZZJQ <zhujinquan@aliyun.com>
2016-06-15 11:36:06 +0800

    创建项目
    创建项目

---
+++ WkMall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WkMall
+//
+//  Created by QUAN on 16/6/15.
+//  Copyright © 2016年 QUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
